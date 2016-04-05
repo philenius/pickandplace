e25981c56c4ad4791e77ab9697ad4671de169859
philenius <philippus-perez@web.de>
2016-04-05 06:53:19 -0700

    added method which reacts on the change of the camera capabilities; commented out logic for saving camera image once a second

--- CameraExtender.cpp
+++ CameraExtender.cpp
@@ -15,42 +15,63 @@ CameraExtender::CameraExtender() {
 CameraExtender::~CameraExtender() {
 }
 
-/**
- * This method handles the COM events and stores the received images on the hard disk.
- * The frames per second (fps) are counted and once per second the image is saved. The old image will be overwritten.
- * The image is saved above the "target" folder as "cameraImage.ppm".
- *
- * @param data
- * @param dataSize
- * @param width
- * @param height
- * @param step
- */
+void CameraExtender::settingsChangedEvent(unsigned int width,
+		unsigned int height, const char * format) {
+	cout << LINE << endl;
+	cout << "Image width: " << width << endl;
+	cout << "Image height: " << height << endl;
+	cout << LINE << endl;
+}
+
+void CameraExtender::capabilitiesChangedEvent(
+		const CameraCapabilities & capibilites) {
+	cout << LINE << endl;
+	cout << "The connected camera supports the following settings:" << endl;
+
+	cout << TAB << "auto white balance: " << capibilites.autoWhiteBalance
+			<< endl;
+	cout << TAB << "auto exposure: " << capibilites.autoExposure << endl;
+	cout << TAB << "auto focus: " << capibilites.autoFocus << endl;
+
+	cout << TAB << "backlight compensation: "
+			<< capibilites.backlightCompensation << endl;
+	cout << TAB << "brightness: " << capibilites.brightness << endl;
+	cout << TAB << "contrast: " << capibilites.contrast << endl;
+	cout << TAB << "exposure: " << capibilites.exposure << endl;
+	cout << TAB << "focus: " << capibilites.focus << endl;
+	cout << TAB << "gain: " << capibilites.gain << endl;
+	cout << TAB << "saturation: " << capibilites.saturation << endl;
+	cout << TAB << "sharpness: " << capibilites.sharpness << endl;
+	cout << TAB << "white balance temperature: "
+			<< capibilites.whiteBalanceTemperature << endl;
+	cout << LINE << endl;
+}
+
 void CameraExtender::imageReceivedEvent(const unsigned char* data,
 		unsigned int dataSize, unsigned int width, unsigned int height,
 		unsigned int step) {
 
-	// Counter for the stored images. Gets reseted every time when the value of fps is reached.
-	static unsigned int sequence = 0;
-	// Counts the amount of calls of this method.
-	static unsigned long countCalls = 0;
-	// Stores the start time of this program
-	static const clock_t beginTime = clock();
-	static unsigned int fps;
-
-	countCalls < ULONG_MAX ? countCalls++ : countCalls = 0;
-
-	// Calculate frames per second
-	float elapsedTime = (clock() - beginTime) / 1000000;
-	fps = countCalls / elapsedTime;
-
-	// Only save the image once per second
-	if (sequence < fps) {
-		sequence++;
-		return;
-	}
-
-	sequence = 0;
+//	// Counter for the stored images. Gets reseted every time when the value of fps is reached.
+//	static unsigned int sequence = 0;
+//	// Counts the amount of calls of this method.
+//	static unsigned long countCalls = 0;
+//	// Stores the start time of this program
+//	static const clock_t beginTime = clock();
+//	static unsigned int fps;
+//
+//	countCalls < ULONG_MAX ? countCalls++ : countCalls = 0;
+//
+//	// Calculate frames per second
+//	float elapsedTime = (clock() - beginTime) / 1000000;
+//	fps = countCalls / elapsedTime;
+//
+//	// Only save the image once per second
+//	if (sequence < fps) {
+//		sequence++;
+//		return;
+//	}
+//
+//	sequence = 0;
 
 	ostringstream outputStream;
 	outputStream << "cameraImage" << ".ppm";
--- CameraExtender.h
+++ CameraExtender.h
@@ -7,6 +7,8 @@
 
 #ifndef CAMERAEXTENDER_H_
 #define CAMERAEXTENDER_H_
+#define TAB "\t"
+#define LINE "#############################################################"
 
 #include <sstream>
 #include <stdio.h>
@@ -25,8 +27,34 @@ public:
 	CameraExtender();
 	virtual ~CameraExtender();
 private:
+	/**
+	 * This method handles the COM events for incoming images and stores the received images on the hard disk.
+	 * The image is saved above the "target" folder as "cameraImage.ppm".
+	 *
+	 * @param data
+	 * @param dataSize
+	 * @param width
+	 * @param height
+	 * @param step
+	 */
 	void imageReceivedEvent(const unsigned char* data, unsigned int dataSize,
 			unsigned int width, unsigned int height, unsigned int step);
+
+	/**
+	 * Reacts when the camera capabilities changed. This event happens also every time once on startup of the program.
+	 * Prints out the camera capabilities.
+	 * @param capabilities
+	 */
+	void capabilitiesChangedEvent(const CameraCapabilities & capabilities);
+
+	/**
+	 * Reacts when the size of the camera image was changed and prints the new size onto the console.
+	 * @param width
+	 * @param height
+	 * @param format
+	 */
+	void settingsChangedEvent(unsigned int width, unsigned int height,
+			const char * format);
 
 };
 
