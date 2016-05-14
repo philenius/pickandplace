1e29806a829435d405d3fed6136edc0f2f3bd913
LukasMarschar <lukasmarschar@live.com>
2016-05-14 01:34:17 -0700

    Main adapted to use controller and observer pattern (old code kept as comment)

--- main.cpp
+++ main.cpp
@@ -3,67 +3,73 @@
 #include <iostream>
 #include <stdlib.h>
 #include "rec/robotino/api2/all.h"
-#include "MyCom.h"
-#include "CameraExtender.h"
-#include "CylinderDetection.h"
-//#include "DistanceWrapper.h"
-//#include "OdometryWrapper.h"
+//#include "MyCom.h"
+//#include "CameraExtender.h"
+//#include "CylinderDetection.h"
+//#include "RelativeRobotPosition.h"
+#include "Controller.h"
 
 using namespace rec::robotino::api2;
 using namespace pickandplace;
 using namespace std;
 
-MyCom com;
-CameraExtender camera;
-//DistanceWrapper distances;
-//OdometryWrapper odometry;
+//MyCom com;
+//CameraExtender camera;
 
-void init(const string& hostname) {
-	// Connect
-	cout << "Connecting...";
-	com.setAddress(hostname.c_str());
-
-	com.connectToServer(true);
-
-	if (false == com.isConnected()) {
-		cout << endl << "Could not connect to " << com.address()
-				<< endl;
-
-		shutdown();
-		exit(1);
-	} else {
-		cout << "success" << endl;
-	}
-}
-
-void destroy() {
-	com.disconnectFromServer();
-}
+//void init(const string& hostname) {
+//	// Connect
+//	cout << "Connecting...";
+//	com.setAddress(hostname.c_str());
+//
+//	com.connectToServer(true);
+//
+//	if (false == com.isConnected()) {
+//		cout << endl << "Could not connect to " << com.address()
+//				<< endl;
+//
+//		shutdown();
+//		exit(1);
+//	} else {
+//		cout << "success" << endl;
+//	}
+//}
+//
+//void destroy() {
+//	com.disconnectFromServer();
+//}
 
 int main(int argc, char **argv) {
+	Controller * controller = new Controller();
+	controller->rotationTest();
 
-	string hostname = "172.26.1.1";
-	if (argc > 1) {
-		hostname = argv[1];
-	}
-
-	try {
-		init(hostname);
-
-		while (com.isConnected()) {
-			com.processEvents();
-		}
-
-		destroy();
-		waitForKey();
-	} catch (const RobotinoException& e) {
-		cerr << "Robotino Error: " << e.what() << endl;
-	} catch (const exception& e) {
-		cerr << "Error: " << e.what() << endl;
-	} catch (...) {
-		cerr << "Unknow Error" << endl;
-	}
-
-	cout << "Press any key to exit..." << endl;
-	waitForKey();
+//	string hostname = "172.26.1.1";
+//	if (argc > 1) {
+//		hostname = argv[1];
+//	}
+//
+//	try {
+//		init(hostname);
+//
+//		int pixelDif;
+//		float leftPercentage;
+//		float rightPercentage;
+//		RelativeRobotPosition position;
+//
+//		while (com.isConnected()) {
+//			com.processEvents();
+//			position.getPosition(&pixelDif, &leftPercentage, &rightPercentage);
+//		}
+//
+//		destroy();
+//		waitForKey();
+//	} catch (const RobotinoException& e) {
+//		cerr << "Robotino Error: " << e.what() << endl;
+//	} catch (const exception& e) {
+//		cerr << "Error: " << e.what() << endl;
+//	} catch (...) {
+//		cerr << "Unknow Error" << endl;
+//	}
+//
+//	cout << "Press any key to exit..." << endl;
+//	waitForKey();
 }
